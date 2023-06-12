76da1f751184823812b4eb1bf9109931f6ca308d
Adams Fontil <adamsfuntil@gmail.com>
2023-06-11 22:06:49 -0400

    finish 9.14

---
+++ my-app/src/components/Total.tsx
@@ -0,0 +1,23 @@
+interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+
+interface TotalProps {
+  courseParts: CoursePart[];
+}
+
+
+const Total = (props: TotalProps)  => {
+  return (
+    <p>
+        Number of exercises{" "}
+        {props.courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </p>
+  )
+};
+
+
+
+export default Total
